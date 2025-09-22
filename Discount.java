a0e704c52d971145c9c11377ed712949ba4aece3
Margaret-irungu-2025 <margaretirungu998@gmail.com>
2025-09-22 14:42:30 +0300

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,31 @@
+// Program to compute discount
+//Name:MARGARET NJERI IRUNGU
+//REG NO:S10-6336-2020
+import java.util.Scanner;
+public class Discount{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double discount;
+        //prompt user to enter amount 
+        System.out.println("Enter the amount purchased: ");
+        double amount = scanner.nextDouble() ;   //nextInt, nextLine, nextFloat
+
+
+        if ( amount > 5000){
+            discount=amount * 0.1 ;
+        }
+        else if (amount >1000 && amount <= 5000){
+            discount =amount * 0.05 ;
+        }
+        else{
+            discount =0;
+        }
+    
+        double amountToPay   = amount -discount;
+        System.out.println( "initial amount " + amount);
+        System.out.println( "Discount " + discount);
+        System.out.println( "Amount to Pay " + amountToPay);
+
+
+    }
+}
