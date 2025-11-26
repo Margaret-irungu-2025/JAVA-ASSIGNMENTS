8f7b6969089c66ee815acb746afeebad92ef3637
Margaret-irungu-2025 <margaretirungu998@gmail.com>
2025-11-26 12:35:15 +0300

    Add files via upload

---
+++ FineRate.java
@@ -0,0 +1,39 @@
+
+
+import java.util.Scanner;
+public class FineRate{
+    public static void main(String[] args){
+        Scanner scanner =new Scanner (System.in);
+        //prompts user to enter book id.due date and return date
+        System.out.println("Enter book ID");
+        int bookID =scanner.nextInt();
+        System.out.println("Enter Due date");
+        int duedate=scanner.nextInt();
+        System.out.println("Enter return date");
+        int returndate=scanner.nextInt();
+        int overdue=returndate-duedate;
+        System.out.println("overdue days are:" +overdue);
+        int fineAmount=scanner.nextInt();
+    
+         int fineRate;
+         fineAmount=overdue* fineRate;
+        System.out.println("Your fine amount is:" + fineAmount + "kshs");
+        //To calculate FineRate
+    
+        if(overdue<=7){
+    
+            fineAmount=20*overdue;
+        
+
+        }
+        if(overdue <=14){
+        
+            fineAmount=50*overdue;
+        }
+        else{
+        
+            fineAmount=100*overdue;
+        }
+    
+    }
+}
